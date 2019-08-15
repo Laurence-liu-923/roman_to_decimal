4cf2c1fca935d7f464ee027b42ffc26000db7421
Lixxday <corentin.normant@gmail.com>
2019-08-15 18:37:59 +0200

    Add tests for roman_to_decimal conversion

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pytest
 from main import verify_input
+from decimal_roman_table import decimal_roman_table
+from converter import roman_to_decimal
 
 def test_input_contains_only_roman_number():
     assert verify_input("IVXLCDM") == "IVXLCDM"
@@ -7,3 +9,8 @@ def test_input_contains_only_roman_number():
 def test_wrong_input_calls_for_error():
     with pytest.raises(ValueError, match="This is not a correct roman number"):
         verify_input("AIV")
+
+@pytest.mark.parametrize('decimal, roman', decimal_roman_table)
+
+def test_converter(roman, decimal):
+    assert roman_to_decimal(roman) == decimal
